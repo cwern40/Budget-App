79265e3ec080eb2601e125dadc2bbe4c5fcff5d5
cwern40 <chris.wernli@hotmail.com>
2020-07-23 22:22:35 -0600

    I added the state for the password page

--- react-budget/src/Components/Login.js
+++ react-budget/src/Components/Login.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "../Styled-Components/Form"
 
 const Login = (props) => {
+
+    const [user, setUser] = useState({username: "", password: ""})
+
+    const handleChange = event => {
+        setUser({ ...user, [event.target.name]: event.target.value });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(user)
+    }
+
     return (
-        <Form>
+        <Form onSubmit={(event) => handleSubmit(event)}>
             <h2>Sign In To Your Account</h2>
             <label>Email
-                <input></input>
+                <input type="email" name="username" placeholder="email@test.com" value={user.username} onChange={event => handleChange(event)} required></input>
             </label>
             <label>Password
-                <input></input>
+                <input type="password" name="password" placeholder="Password" value={user.password} onChange={event => handleChange(event)} required></input>
             </label>
+            <button type="submit">Login</button>
         </Form>
     );
 }
