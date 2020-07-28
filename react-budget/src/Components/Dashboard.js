d50347f6a7a537a2bc36a00b594beddbb0de5b5f
cwern40 <chris.wernli@hotmail.com>
2020-07-27 22:21:04 -0600

    I added the PieChart as a router to the Dashboard component

--- react-budget/src/Components/Dashboard.js
+++ react-budget/src/Components/Dashboard.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
+import {BrowserRouter, Router } from 'react-router-dom';
+import PieChart from './Pie-Chart';
 
 const Dashboard = (props) => {
     return (
         <div>
-
+            <BrowserRouter>
+                <Router exact path="/" component={PieChart} />
+            </BrowserRouter>
         </div>
     )
 }
