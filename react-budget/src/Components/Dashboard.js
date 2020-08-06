3c43d2a23bcf1f71c4c7cd2fc41551f751446b3c
cwern40 <chris.wernli@hotmail.com>
2020-08-05 20:56:37 -0600

    I changed the router path to display the Budget instead of the PieChart

--- react-budget/src/Components/Dashboard.js
+++ react-budget/src/Components/Dashboard.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import {BrowserRouter, Router } from 'react-router-dom';
-import PieChart from './Pie-Chart';
+import Budget from './Budget';
 
 const Dashboard = (props) => {
+
+    const [revenue, setRevenue] = useState([]);
+    const [expenses, setExpenses] = useState([]);
+
     return (
         <div>
             <BrowserRouter>
-                <Router exact path="/" component={PieChart} />
+                <Router exact path="/" component={Budget} revenue={revenue} expenses={expenses}/>
             </BrowserRouter>
         </div>
     )
